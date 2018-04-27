7c392f54a6aefabe62ad906c9d8168fe9a945233
309 <16space12@sdh.hs.kr>
2018-04-27 15:59:16 +0900

    js 코드 수정

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
-window.onload = function () {
-    start()
-}
+window.onload = start;
 
 function start() {
     var load = document.getElementById("load");
     var wrap = document.getElementById("wrap");
 
-    fadeOut(load);
-    fadeIn(wrap);
+    fadeInOut(load, wrap);
 }
 
-function fadeOut(load) {
-    setInterval(function () {
-        load.style.animationName = "out";
-        load.style.animationDuration = "3s";
-        load.style.display = "none";
-    }, 1000);
-    clearInterval();
-}
-
-function fadeIn(wrap) {
+function fadeInOut(load, wrap) {
     setInterval(function () {
         wrap.style.animationName = "in";
         wrap.style.animationDuration = "3s";
         wrap.style.display = "block";
+        load.style.display = "none";
     }, 1000);
     clearInterval();
 }
